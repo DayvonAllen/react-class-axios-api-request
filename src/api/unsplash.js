f9caca1997f810c15efbde730b2c943e731864de
DayvonAllen <dayvonallen93@outlook.com>
2021-11-14 10:51:45 -0600

    added notes

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
+// creates an instance of the axios client with a couple of default properties
+// can be reused all over the react app
 export default axios.create({
-  baseURL: 'https://api.unsplash.com',
+  baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
-      'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296',
+      "Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296",
   },
 });
